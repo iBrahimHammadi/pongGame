92fe2d8f8d177eea8d28c4cec45ce974d7d9015b
iBrahimHammadi <ibrahimsh2091@gmail.com>
2023-10-07 09:33:55 +0100

    Added the player animation with it's cllisions

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= height:
+        ball_speed_y *= -1
+    if ball.right >= width or ball.left <= 0:
+        ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+        
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= height:
+        player.bottom = height
+
 pygame.init()
 clock = pygame.time.Clock()
 width, height = 800, 600
@@ -18,6 +36,7 @@ bg_color = pygame.Color('grey12')
 #Adding the ball velocity
 ball_speed_x = 5
 ball_speed_y = 5
+player_speed = 0
 
 #Creating the game loop
 run = True
@@ -27,13 +46,19 @@ while run:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    if ball.top <= 0 or ball.bottom >= height:
-        ball_speed_y *= -1
-    if ball.right >= width or ball.left <= 0:
-        ball_speed_x *= -1
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 5
+            if event.key == pygame.K_UP:
+                player_speed -= 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 5
+            if event.key == pygame.K_UP:
+                player_speed += 5
+
+    player_animation()
+    ball_animation()
 
     Screen.fill(bg_color)
     pygame.draw.rect(Screen, light_grey, player)
